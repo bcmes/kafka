201a96bd6432f0441110768ce5052e8edcc42118
brunomeloesilva <brunomeloesilva@gmail.com>
2021-06-30 15:40:02 -0400

    Registro das analises de estudo

---
+++ service-email/src/main/java/github/com/brunomeloesilva/ecommerce/email/ServiceEmail.java
@@ -0,0 +1,33 @@
+package github.com.brunomeloesilva.ecommerce.email;
+
+import java.util.HashMap;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import github.com.brunomeloesilva.ecommerce.ConsumidorKafka;
+
+public class ServiceEmail {
+	public static void main(String[] args) {
+		
+		var service = new ServiceEmail();
+		
+		try(var serviceKafka = new ConsumidorKafka<Email>(ServiceEmail.class.getName()
+											, "ECOMMERCE_SEND_EMAIL"
+											, service::parse
+											, Email.class
+											, new HashMap<>()))
+		{
+			serviceKafka.run();
+		}
+		
+	}
+	
+	private void parse(ConsumerRecord<String, Email> record) {
+		System.out.println("\n--------------------- INICIO DO PROCESSAMENTO DA MENSAGEM ---------------------");
+		var mensagem =  String.format("Enviando Email: Key = %s, Value = %s, Partition = %s, Offset = %s"
+								, record.key(), record.value(), record.partition(), record.offset());
+		System.out.println(mensagem);
+		try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); } // Só para simular a demora de um processo.
+		System.out.println("--------------------- FIM DO PROCESSAMENTO DA MENSAGEM ---------------------\n");
+	}
+}
